c92865a7f30421ed0ee933e388d28fcbe0c630ab
Fallrainy <nomeqc@gmail.com>
2019-09-17 00:04:15 +0800

    update user rule

--- gen_list.py
+++ gen_list.py
@@ -5,8 +5,8 @@ import requests, base64
 
 
 def gen_base64(s):
+
     b64_str = str(base64.b64encode(bytes(s, 'utf-8')), 'utf-8')
-    # print(b64_str)
     ret_str = ""
     index = 0
     s_len = len(b64_str)
@@ -31,14 +31,13 @@ if __name__ == "__main__":
 
     print("正在下载gfwlist.txt ...")
     try:
-        response = requests.get("https://pagure.io/gfwlist/raw/master/f/gfwlist.txt")
+        response = requests.get("https://bitbucket.org/gfwlist/gfwlist/raw/HEAD/gfwlist.txt")
         gfwlist = base64.b64decode(response.text)
     except Exception:
         print("请求出错了")
     
     with open("list.txt", "w", encoding="utf-8") as f:
-        result = '!################Fallrainy Start##################\n{}!################Fallrainy End##################\n\n{}'.format(user_rule, str(gfwlist, 'utf-8'))
-        # print("result:" + result)
+        result = '!################Fallrainy Start##################\n{}\n!################Fallrainy End##################\n\n{}'.format(user_rule, str(gfwlist, 'utf-8'))
         content = gen_base64(result)
         f.write(content)
     print("list.txt已经生成成功。")
